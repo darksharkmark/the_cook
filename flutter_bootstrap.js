e55c775f9e3c5b71f6a5af729a166cfc29eb2d36
darksharkmark <darksharkmark@users.noreply.github.com>
2025-10-07 07:31:17 +0000

    deploy: 856369389d3abdd6ec826961c60f05210722cf5b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "885580049"
+    serviceWorkerVersion: "2936176679"
   }
 });
